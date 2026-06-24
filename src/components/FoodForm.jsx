73a73fe196394b26f731f6f72269718134b87e64
AMIKO <akozmanishvili@gmail.com>
2026-06-24 16:23:43 +0400

    Got FoodForm component ready,which creates form for calories, name, protein for user to input. Also started working on FoodList component

--- src/components/FoodForm.jsx
+++ src/components/FoodForm.jsx
@@ -1,5 +1,78 @@
-const FoodForm = () => {
-  return <div>FoodForm</div>;
+import { useState } from "react";
+
+const FoodForm = ({ onAddFood }) => {
+  const [name, setName] = useState(``);
+  const [calories, setCalories] = useState(``);
+  const [proteins, setProteins] = useState(``);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newFood = {
+      id: Date.now(),
+      name,
+      calories: parseInt(calories),
+      proteins: parseInt(proteins),
+    };
+
+    onAddFood(newFood);
+
+    setName(``);
+    setCalories(``);
+    setProteins(``);
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          padding: "8px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          marginBottom: "20px",
+          alignItems: "center",
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          style={{ padding: "8px", width: "600px" }}
+        />
+        <input
+          type="number"
+          placeholder="Calories..."
+          value={calories}
+          onChange={(e) => setCalories(e.target.value)}
+          style={{ padding: "8px", width: "600px" }}
+        />
+        <input
+          type="number"
+          placeholder="Proteins..."
+          value={proteins}
+          onChange={(e) => setProteins(e.target.value)}
+          style={{ padding: "8px", width: "600px" }}
+        />
+        <button
+          type="submit"
+          style={{
+            padding: "10px",
+            background: "#007bff",
+            color: "white",
+            border: "none",
+            borderRadius: "4px",
+            cursor: "pointer",
+            width: "600px",
+          }}
+        >
+          Add Food
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default FoodForm;
--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -1,5 +1,16 @@
-const FoodList = () => {
-  return <div>FoodList</div>;
+const FoodList = ({ foods }) => {
+  return (
+    <div>
+      {foods.map((food) => {
+        return (
+          <div>
+            {" "}
+            <h3>Log</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FoodList;
